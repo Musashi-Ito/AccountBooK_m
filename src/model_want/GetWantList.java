baf4d32a1623ffc2353ba83ca3fbffe342ad758d
Musashi-Ito <iris0436@outlook.jp>
2018-01-22 12:26:39 +0900

    add

---
+++ src/model_want/GetWantList.java
@@ -0,0 +1,50 @@
+package model_want;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.UserMoney;
+
+public class GetWantList implements java.io.Serializable{
+
+	public GetWantList(){}
+
+	public UserMoney execute(int id){
+		Connection con = null;
+		try {
+			Class.forName("org.apache.derby.jdbc.ClientDriver");
+			con = DriverManager.getConnection("jdbc:derby://localhost:1527/sample;create=true", "user", "pass");
+			String sql = "SELECT * FROM wantlist_TBL WHERE ID = ?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			//System.out.println("");
+
+			stmt.setInt(1,id);
+			ResultSet rest = stmt.executeQuery();
+
+			while (rest.next()) {
+				UserMoney um = new UserMoney(id,rest.getInt("START"), rest.getInt("GOAL"));
+				return um;
+			}
+
+			return null;
+			//*/
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+		}
+}
